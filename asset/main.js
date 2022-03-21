76f6df551198efbc84bf8e3a27dd54b03f1e1937
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-21 16:40:24 +0100

    end 1

--- asset/main.js
+++ asset/main.js
@@ -1,5 +1,26 @@
-function randomStuff(min, max){
-  var pcNumber = Math.floor(Math.random() * (max + 1 - min) + min);
-  return pcNumber;
+let grid = document.getElementById('grid');
+
+for (let i = 1; i <= 100; i++) {
+  // document.getElementById('grid').innerHTML += <div id="box${i}" class="box"></div>;
+  let box = document.createElement('div');
+  box.innerHTML += box.innerHTML += `<div>${i}</div>`;
+  box.classList.add('box');
+  grid.appendChild(box);
+
+
+  box.addEventListener('click', function () {
+    console.log(this);
+    // this.classList.add('clicked');
+    console.log(this.innerHTML);
+    let numero = this.innerHTML;
+
+    if (numero % 2 === 0) {
+      box.classList.add('boxLightBlue');
+    } else {
+      box.classList.add('boxLightBlue');
+    }
+
+  });
+
 }
 
